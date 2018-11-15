ee38f6deddec70175ae4dd12687d5702d8b294ad
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-15 14:52:21 +0100

    Update .vue for spinner-div

--- resources/assets/js/components/nested-sets.vue
+++ resources/assets/js/components/nested-sets.vue
@@ -29,47 +29,164 @@
             </ul>
         </div>
 
-        <!-- Add node -->
-        <div class="tree-section add-node">
+        <div v-if="loading" id="overlay">
             <div>
-                <button
-                    type="button"
-                    class="btn btn-primary btn-sm"
-                    @click="appendNode"
-                >
-                    Append node to
-                </button>
-                <input
-                    id="add-node"
-                    class="form-control"
-                    type="text"
-                    v-model="appendNodeId"
-                />
+                <vue-simple-spinner size="large"></vue-simple-spinner>
+                <h4 class="load-inter-message">Loading ...</h4>
             </div>
         </div>
 
-        <!-- Delete non-root node -->
-        <div class="tree-section delete-node">
-            <div>
-                <button
-                    type="button"
-                    class="btn btn-primary btn-sm"
-                    @click="deleteNode"
-                >
-                    Delete node
-                </button>
-                <input
-                    id="delete-node"
-                    class="form-control"
-                    type="text"
-                    v-model="deleteNodeId"
-                />
+        <div>
+            <!-- Random node -->
+            <div class="tree-section check-tree">
+                <div>
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        @click="checkTree"
+                    >
+                        Check tree
+                    </button>
+                </div>
+            </div>
+
+            <!-- Random node -->
+            <div class="tree-section random-node">
+                <div>
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        @click="randomNode"
+                    >
+                        Get random node info
+                    </button>
+                </div>
+            </div>
+
+            <!-- Random leaf -->
+            <div class="tree-section random-leaf">
+                <div>
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        @click="randomLeaf"
+                    >
+                        Get random leaf info
+                    </button>
+                </div>
+            </div>
+
+            <!-- Add node -->
+            <div class="tree-section add-node">
+                <div>
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        @click="appendNode"
+                    >
+                        Append node
+                    </button>
+                    <input
+                        id="add-node"
+                        class="form-control"
+                        type="text"
+                        placeholder="Node id"
+                        v-model="appendNodeId"
+                    />
+                </div>
+            </div>
+
+            <!-- Delete non-root node -->
+            <div class="tree-section delete-node">
+                <div>
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        @click="deleteNode"
+                    >
+                        Delete node
+                    </button>
+                    <input
+                        id="delete-node"
+                        class="form-control"
+                        type="text"
+                        placeholder="Node id"
+                        v-model="deleteNodeId"
+                    />
+                </div>
+            </div>
+
+            <!-- copy non-root node -->
+            <div class="tree-section copy-node">
+                <div>
+                    <button
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        @click="copyNode"
+                    >
+                        Copy node
+                    </button>
+                    <input
+                        id="copy-node-self-id"
+                        class="form-control"
+                        type="text"
+                        placeholder="Copy id"
+                        v-model="copyNodeId"
+                    />
+                    <input
+                        id="copy-node-parent-id"
+                        class="form-control"
+                        type="text"
+                        placeholder="Parent id"
+                        v-model="copyNodeParentId"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <p>Random node info:</p>
+            <div v-if="randomNodeInfo !== null">
+                <span class="dataEntry">id: </span><span>{{ randomNodeInfo.id }}</span><br/>
+                <span class="dataEntry">title: </span><span>{{ randomNodeInfo.title }}</span><br/>
+                <span class="dataEntry">parent_id: </span><span>{{ randomNodeInfo.parent_id }}</span><br/>
+                <span class="dataEntry">updated_at: </span><span>{{ randomNodeInfo.updated_at }}</span><br/>
+                <span class="dataEntry">_lft: </span><span>{{ randomNodeInfo._lft }}</span><br/>
+                <span class="dataEntry">_rgt: </span><span>{{ randomNodeInfo._rgt }}</span><br/>
+                <span class="dataEntry">path: </span><span>{{ randomNodeInfo.path }}</span>
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss">
+    #overlay {
+        position: fixed; /* Sit on top of the page content */
+        display: block; /* Hidden by default */
+        width: 100%; /* Full width (cover the whole page) */
+        height: 100%; /* Full height (cover the whole page) */
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+        z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+        cursor: pointer; /* Add a pointer on hover */
+
+        h4 {
+            text-align: center;
+            color: white;
+        }
+
+        div {
+            margin-top: 200px;
+        }
+    }
+
+    .dataEntry {
+        font-weight: bold;
+    }
+
     .tree-container {
         padding: 20px;
     }
