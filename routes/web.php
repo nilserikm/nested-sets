4797043a34afc6bc179efc59e334da7ffe5718af
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-11-15 14:46:16 +0100

    Add a bunch of routes and changed createNode to appendNode

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function() {
     return view('nested-sets');
 });
 
+Route::post('/node/random/leaf', 'WelcomeController@randomLeaf');
+Route::post('/node/random/node', 'WelcomeController@randomNode');
+Route::post('/node/copy', 'WelcomeController@copyNode');
 Route::post('/node/delete', 'WelcomeController@deleteNode');
-Route::post('/node/create', 'WelcomeController@createNode');
+Route::post('/node/append', 'WelcomeController@appendNode');
 Route::get('/tree/fetch', 'WelcomeController@fetchTree');
+Route::post('/tree/check', 'WelcomeController@checkTree');
